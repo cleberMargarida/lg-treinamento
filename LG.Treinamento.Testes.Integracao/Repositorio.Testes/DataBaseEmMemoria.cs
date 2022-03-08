709d8c1c9bc225a9f77a2cd8f7af2b7880d3eb4b
cleberMargarida <cleberfilho3@gmail.com>
2022-03-08 09:37:11 -0300

    [Autor: Diego] Estagiario Teste Fixture

---
+++ LG.Treinamento.Testes.Integracao/Repositorio.Testes/DataBaseEmMemoria.cs
@@ -0,0 +1,46 @@
+﻿using FluentNHibernate.Cfg;
+using NHibernate;
+using NHibernate.ByteCode.Castle;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Tool.hbm2ddl;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LG.Treinamento.Testes.Integracao.Repositorio.Testes
+{
+    public class DataBaseEmMemoria
+    {
+        protected ISession session;
+        private ISessionFactory sessionFactory;
+        private Configuration configurationHibernate;
+
+        public DataBaseEmMemoria(Assembly assemblyContendoMapeamento)
+        {
+            configurationHibernate = new Configuration()
+                    .SetProperty(Environment.ReleaseConnections, "on_close")
+                    .SetProperty(Environment.Dialect, typeof(SQLiteDialect).AssemblyQualifiedName)
+                    .SetProperty(Environment.ConnectionDriver, typeof(SQLite20Driver).AssemblyQualifiedName)
+                    .SetProperty(Environment.ConnectionString, "data source=:memory:")
+                    .SetProperty(Environment.ProxyFactoryFactoryClass, typeof(ProxyFactoryFactory).AssemblyQualifiedName);
+            sessionFactory = sessionFactory = Fluently.Configure(configurationHibernate)
+                            .Mappings(
+                            m => m
+                            .FluentMappings
+                            .AddFromAssembly(assemblyContendoMapeamento))
+                            .BuildSessionFactory();
+
+            session = sessionFactory.OpenSession();
+            new SchemaExport(configurationHibernate).Execute(true, true, false, session.Connection, System.Console.Out);
+        }
+
+        ~DataBaseEmMemoria()
+        {
+            session.Dispose();
+        }
+    }
+}
---
+++ LG.Treinamento.Testes.Integracao/Repositorio.Testes/EstagiarioTesteFixture.cs
@@ -0,0 +1,20 @@
+﻿using LG.Treinamento.ServicoMapeador.Mapeadores.Mapeamentos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LG.Treinamento.Testes.Integracao.Repositorio.Testes
+{
+    public class EstagiarioTesteFixture : DataBaseEmMemoria
+    {
+        public EstagiarioTesteFixture() : base(typeof(EstagiarioMap).Assembly)
+        {
+            
+        }
+
+
+    }
+}
