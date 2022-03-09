98bf8c576eca3abe71a3621622e5896c08d2c664
anvmano <anvmano@gmail.com>
2022-03-09 11:58:57 -0300

    Definicao de servicos

---
+++ LG.Treinamento.ServicoMapeador/Servicos/ContratosImplementados/ServiceEstagiario.cs
@@ -0,0 +1,38 @@
+﻿using LG.Treinamento.InterfacesFabricas.ContratosDeServicos.Dados;
+using LG.Treinamento.InterfacesFabricas.ContratosDeServicos.Servicos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LG.Treinamento.ServicoMapeador.Servicos.ContratosImplementados
+{
+    public class ServiceEstagiario : IServiceEstagiario
+    {
+        public void Create(DTOEstagiario turma)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(DTOEstagiario turma)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<DTOEstagiario> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DTOEstagiario GetEstagiario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(DTOEstagiario turma)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LG.Treinamento.ServicoMapeador/Servicos/ContratosImplementados/ServiceTurma.cs
@@ -0,0 +1,38 @@
+﻿using LG.Treinamento.InterfacesFabricas.ContratosDeServicos.Dados;
+using LG.Treinamento.InterfacesFabricas.ContratosDeServicos.Servicos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LG.Treinamento.ServicoMapeador.Servicos.ContratosImplementados
+{
+    public class ServiceTurma : IServiceTurma
+    {
+        public void Create(DTOTurma turma)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(DTOTurma turma)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<DTOTurma> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DTOTurma GetTurma(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(DTOTurma turma)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
